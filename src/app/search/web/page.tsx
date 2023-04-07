239a604602abdd0ba1406f819c7eabca10588f77
Knud Rishøj <tirdyr@tobenski.dk>
2023-04-07 21:56:02 +0200

    Fetch data from google search api

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,18 @@
-const WebSearchPage = () => {
+const WebSearchPage = async ({searchParams}:{searchParams:any}) => {
+    const response = await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=
+        ${process.env.SEARCH_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    )
+    const data = await response.json();
+    const results = data.items;
+    
     return (
         <div>
-            WebSearchPage
+            { results && results.map((result:any, i:number) => {
+                return (
+                    <h1 key={i.toString()}>{result.title}</h1>
+                )
+            })}
         </div>
     );
 }
