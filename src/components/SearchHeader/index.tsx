cb9a560cb4bc4239b5deb99c82c26e271235fa8c
Knud Rishøj <tirdyr@tobenski.dk>
2023-04-07 20:59:01 +0200

    update serachheader comp

--- src/components/SearchHeader/index.tsx
+++ src/components/SearchHeader/index.tsx
@@ -1,8 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from "../SearchBox";
+import { RiSettings3Line } from 'react-icons/ri'
+import { TbGridDots } from 'react-icons/tb'
+import SearchHeaderOptions from "../SearchHeaderOptions";
+
 const SearchHeader = () => {
     return (
-        <div>
-            SearchHeader
-        </div>
+        <header className="sticky top-0 bg-white">
+            <div className="flex w-full p-6 items-center justify-between">
+                <Link href='/'>
+                    <Image width={120} height={40} src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png' alt="" />
+                </Link>
+                <div className="flex-1">
+                    <SearchBox />
+                </div>
+                <div className="hidden md:inline-flex space-x-2">
+                    <RiSettings3Line className="header-icon"/>
+                    <TbGridDots className="header-icon" />
+                </div>
+                <button className="bg-blue-500 text-white 
+                                    px-6 py-2 font-medium rounded-md 
+                                    hover:brightness-105 hover:shadow-md transition-all ml-2">Sign in</button>
+            </div>
+            <SearchHeaderOptions />
+        </header>
     );
 }
 
---
+++ src/components/SearchHeaderOptions/index.tsx
@@ -0,0 +1,9 @@
+const SearchHeaderOptions = () => {
+    return (
+        <div>
+            SearchHeaderOptions
+        </div>
+    );
+}
+
+export default SearchHeaderOptions;
