b64695fd043c5a328968ad2ecca636a48876e5dc
Knud Rishøj <tirdyr@tobenski.dk>
2023-04-07 14:33:34 +0200

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'Google',
   description: 'Google Clone created by nextjs 13',
+  icons: {
+    icon: '/favicon.svg',
+  }
 }
 
 export default function RootLayout({
